dde157e5cf029241f772542304ae620a05672f8f
Kenta-kenpy <101815272+Kenta-kenpy@users.noreply.github.com>
2024-06-13 17:45:19 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+</head>
+<body>
+  <script>
+    // JavaScriptでHTML要素を生成する
+    var h1 = document.createElement('h1');
+    h1.className = 'title';
+    h1.textContent = 'JavaScript Paizaラーニング';
+    document.body.appendChild(h1);
+
+    var a = document.createElement('a');
+    a.href = 'https://github.com/Kenta-kenpy/paiza-Java';
+    a.textContent = 'JavaScript　標準入力備忘録';
+    document.body.appendChild(a);
+  </script>
+</body>
+</html>
